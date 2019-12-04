2717aef20187f1d4d89555903938eb420a5bef1a
Elamparuthi Sivarasu Kavandappan (ELAM) <elampsiv@microsoft.com>
2019-12-04 09:51:06 +0530

    Add project files.

---
+++ OrderRead/Infra/ServiceBusReciver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace OrderRead.Infra
+{
+    public interface IServiceBusReceiver
+    {
+        void ReceiveMessage();
+
+    }
+    public class ServiceBusReceiver : IServiceBusReceiver
+    {
+        private string _connectionstring;
+        private string _topic;
+        private string _subscriptionname;
+        private SubscriptionClient client;
+
+        public ServiceBusReceiver(string connectionstring, string topic, string subscriptionname)
+        {
+            _connectionstring = connectionstring;
+            _topic = topic;
+            _subscriptionname = subscriptionname;
+            client = new SubscriptionClient(_connectionstring, _topic, _subscriptionname);
+        }
+
+        public void ReceiveMessage()
+        {
+            MessageHandlerOptions options = new MessageHandlerOptions(exceptionhandler)
+            {
+                AutoComplete = true,
+                MaxConcurrentCalls = 1
+            };
+            client.RegisterMessageHandler(getmessage, options);
+        }
+
+        private Task getmessage(Message arg1, CancellationToken arg2)
+        {
+            string inmsg = System.Text.Encoding.ASCII.GetString(arg1.Body);
+            dynamic obj = JsonConvert.DeserializeObject<dynamic>(inmsg);
+
+            switch (obj.EventType.ToString())
+            {
+                case "OrderCreated":
+                    break;
+                default:
+                    break;
+            }
+            client.CompleteAsync(arg1.SystemProperties.LockToken).GetAwaiter().GetResult();
+            return Task.CompletedTask;
+        }
+
+        private Task exceptionhandler(ExceptionReceivedEventArgs arg)
+        {
+            var aa = 1;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ OrderRead/ServiceBusHosting.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OrderRead.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderRead
+{
+    public class ServiceBusHosting : IHostedService
+    {
+        private IServiceProvider _provider;
+        public ServiceBusHosting(IServiceProvider serviceprovider)
+        {
+            _provider = serviceprovider;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            using(var scope = _provider.CreateScope())
+            {
+                IServiceBusReceiver rec = scope.ServiceProvider.GetRequiredService<IServiceBusReceiver>();
+                rec.ReceiveMessage();
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            var aa = 1;
+            return Task.CompletedTask;
+        }
+    }
+}
